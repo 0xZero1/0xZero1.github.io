dd0c039c30caf4eb8dd758abf03ad578f1f94ae2
zero <moosesolutions@protonmail.com>
2023-01-22 13:09:00 -0600

    Modified website to a customer template.

---
+++ script/page.js
@@ -0,0 +1,10 @@
+// Javascript for website
+
+
+// Signup/ contact us function with popup window to input information
+function contact() {
+
+}
+
+const contactButton = document.querySelector('#signup');
+contactButton.addEventListener('click', contact);
